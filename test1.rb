f40d0c4418c08f6d3c828dd3ed70e6a4a51bffc9
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2020-11-27 15:06:20 +0900

    first commit

---
+++ test1.rb
@@ -0,0 +1,14 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/task', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya_false.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_false.rb')
+server.start
